1b88f96304740bd321b98a6d80b5e0da6120a4ca
akaslayer <hendrysurijanto123@gmail.com>
2021-10-20 15:04:44 +0700

    ganti font family

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="css/index.css" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
 	<title>Document</title>
-	
 </head>
 <body>
-	<img src="images/Depression 1.png">
+	<div class="row">
+	<div class="col">
+	<img src="images/Depression 1.png" class="center">
+</div>
+<div class="col">
+	<h1>TES PARAMETER DEPRESI ANDA</h1>
+</div>
+</div>
 </body>
 </html>
