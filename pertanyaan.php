49b39f4ad58a2ab9a80dbcb8c0de20ece240c6de
steven278 <darkavenger347@gmail.com>
2021-10-20 16:08:35 +0700

    pertanyaan

--- pertanyaan.php
+++ pertanyaan.php
@@ -1,66 +1,56 @@
-<?php
-	// untuk memanggil file
-	include 'Crud.php';
-	// untuk mendeklarasikan class menjadi variabel
-	$crud = new Crud();
-	$arrayName = $crud->readGejala();
- ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php include 'Navbar.php' ?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Sistem Pakar Metode CF (Certainty Factor)</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link href="css/pertanyaanStyle.css" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Roboto&display=swap" rel="stylesheet">
 </head>
-
 <body>
-	<div class="container">
+	<div class="container mt-5">
 		<div class="row">
-		  <div class="col-lg-12">
-				<h1 style="text-align: center;">Sistem Pakar Metode CF (Certainty Factor)</h1>	  	
-		  </div>
-		</div>
-	</div>
-	
-	<div class="container">
-		<div class="col-lg-12">
-			<table width="1000" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#000099">
-			  <tr>
-			    <td align="center" valign="top" bgcolor="#FFFFFF"><br />
-					<form name="form1" method="post" action="hasil.php"><br>
-					  <table align="center" width="600" border="1" cellspacing="0" cellpadding="5">
-					  <tr>
-					  <td id="ignore" bgcolor="#DBEAF5" width="300"><div align="center"><strong><font size="2" face="Arial, Helvetica, sans-serif"><font size="2">GEJALA</font> </font></strong></div></td>
-					  <?php
-					    	foreach ($arrayName as $r)
-						  {
-						?>
-					    <tr>
-					      <td width="600">
-					        <input id="gejala<?php echo $r['id_gejala']; ?>" name="gejala[]" type="checkbox" value="<?php echo $r['id_gejala']; ?>">
-					        <?php echo $r['nama_gejala']; ?><br/>
-					        </td>
-					    </tr>
-					    <?php
-					      }
-					     ?>
-					    <tr>
-					      <td><input type="submit" name="button" value="Proses"></td>
-					    </tr>
-					  </table>
-					  <br>
-					</form>
-				</td>
-			  </tr>
-			</table>
+			<h1 class="mb-5">GEJALA</h1>
+			<div class="col-sm-6 col-md-7">
+				<!-- <table class="table table-light table-bordered border-dark">
+					<thead class="table-primary table-bordered border-dark"> -->
+				<table class="table table-light table-bordered border-dark">
+					<thead class="table-info table-bordered border-dark">
+						<tr>
+							<th scope="col">Pilih Gejala</th>
+							<th scope="col">Gejala</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row"><input type="checkbox" name="" id=""></th>
+							<td>lorem</td>
+						</tr>
+						<tr>
+							<th scope="row"><input type="checkbox" name="" id=""></th>
+							<td>Jacob</td>
+						</tr>
+						<tr>
+							<th scope="row"><input type="checkbox" name="" id=""></th>
+							<td>Jacob</td>
+						</tr>
+						<tr>
+							<th scope="row"><input type="checkbox" name="" id=""></th>
+							<td>Jacob</td>
+						</tr>
+					</tbody>
+				</table>
+				<div class="col-sm-6 col-md-8 d-flex justify-content-center">
+					<button type="submit" id="btn-solusi">Large button</button>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-5 mt-5">
+				<img src="images/Depression 2.png" alt="Depression_image">
+			</div>
 		</div>
 	</div>
 </body>
