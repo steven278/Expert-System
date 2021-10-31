7f829a4b6fa1cbdec0f8ce590ab33fb5454f6169
akaslayer <hendrysurijanto123@gmail.com>
2021-10-31 11:06:21 +0700

    Update HasilDiagnosis.php

--- HasilDiagnosis.php
+++ HasilDiagnosis.php
@@ -11,13 +11,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
+ <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
     <link href="css/hasilDiagnosisStyle.css" rel="stylesheet">
@@ -141,20 +141,20 @@
               <div class="row">
               <div class="col-md-12">
               <table class="table table-light table-bordered border-dark" style="text-align: center;">
-						<thead class="table-info table-bordered border-dark">
-							<tr>
-								<th scope="col">Nama Penyakit</th>
-								<th scope="col">Nilai CF</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<?php   
-                            $crud->hasilCFTertinggi($daftar_cf,$groupKemungkinanPenyakit);
+      <thead class="table-info table-bordered border-dark">
+       <tr>
+        <th scope="col">Nama Penyakit</th>
+        <th scope="col">Nilai CF</th>
+       </tr>
+      </thead>
+      <tbody>
+       <tr>
+        <?php   
+                  $crud->hasilCFTertinggi($daftar_cf,$groupKemungkinanPenyakit);
                 ?>
               </tr>
             </tbody>
-				    </table>
+        </table>
             <h2 id="kemungkinan"> Kemungkinan Penyakit : </h2>
               <ul>  
                 <?php $crud->hasilAkhir($daftar_cf,$groupKemungkinanPenyakit); ?>
